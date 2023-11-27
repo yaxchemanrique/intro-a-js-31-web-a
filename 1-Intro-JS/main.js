a61cbf271354f0b8363a1d1951f063d7dd644d73
Diego Lechuga <diegolechuga2003@gmail.comgit config --list>
2023-11-27 15:30:57 -0600

    feat: Intro a JavaScript

---
+++ 1-Intro-JS/main.js
@@ -0,0 +1,39 @@
+let numero = 5;
+
+console.log(numero);
+
+numero = 10;
+
+console.log(numero);
+
+console.log(10+2);
+
+//alert("Esta página tiene virus");
+
+//let nombreUsuario = prompt("¿Cómo te llamas?");
+
+let fruta;
+
+console.log(fruta);
+
+//document.write("Me llamo " + nombreUsuario + " !");
+
+console.log(5%2);
+
+console.log("");
+
+/* Este codigo es
+una prueba para entender JavaScript */
+
+// Este es un comentario de una sola linea
+
+
+let primerNumero = Number(true);
+
+/*let segundoNumero = parseInt(prompt("Dame otro número"));
+
+let resultado = primerNumero + segundoNumero;
+
+console.log(`Vamos a sumar ${primerNumero} y ${segundoNumero} y el resultado es: ${resultado}`);*/
+
+console.log(primerNumero);
